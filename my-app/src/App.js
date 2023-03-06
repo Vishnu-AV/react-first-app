89f68e6e03b70a066be18997961bcb5b00927009
Vishnu A V <vishnu.av@perfomatix.com>
2023-03-06 23:00:33 +0530

    Importing and Exporting Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Profile } from "./profile";
 
 function App() {
+  const name = "Vishnu";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hi {name}</p>
+      <h1>Hello, {formatName(user)}!</h1>
+      {element}
+      {tick()}
+      <Profile />
     </div>
   );
 }
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
 
+const user = {
+  firstName: "John",
+  lastName: "honay",
+};
+const element = (
+  <h1 className="greeting" style={{ color: "red" }} >
+    Hello, welcome!
+  </h1>
+);
+function tick() {
+  return <div>
+      <h1>Current time</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+}
+setInterval(tick, 1000);
 export default App;
