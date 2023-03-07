24ff00998d87266f9655eb5c7e0256a815186cac
Vishnu A V <vishnu.av@perfomatix.com>
2023-03-07 21:03:32 +0530

    Handling Events

--- second-app/src/details.js
+++ second-app/src/details.js
@@ -1,12 +1,36 @@
 import React from "react";
+import Toggle from './toggle';
 
 export default class Details extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = props.props;
+  constructor(props) {
+    super(props);
+    this.state = props.props;
+  }
+  activateLasers() {
+    alert("Button clicked");
+  }
+  render() {
+    return (
+      <>
+        <p> Child component</p>
+        <h2>It is {this.state.name}.</h2>
+        <h2>It is {this.state.subject}.</h2>
+        <Form />
+        <Toggle />
+        <button onClick={this.activateLasers}>Activate Button</button>
+      </>
+    );
+  }
+}
+function Form() {
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log('You clicked submit.');
     }
-    
-    render() {
-      return <><h2>It is {this.state.name}.</h2><h2>It is {this.state.subject}.</h2></>;
-    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Submit</button>
+      </form>
+    );
   }
