0295cb1bb910d8e220b64f8d4f3fd2a27cf87f6f
Vishnu A V <vishnu.av@perfomatix.com>
2023-03-07 20:39:39 +0530

    Adding Lifecycle Methods to a Class

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    // The only place where we can assign this.state is the constructor.
+    this.state = {
+      date: new Date(),
+      name: "Kutta",
+      names: ["Kutta", "Potta"],
+    };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState(function (state, props) {
+      return {
+        date: new Date(),
+        name: state.names[this.state.date.getSeconds() % 2],
+        counter: props.data,
+      };
+    });
   }
   render() {
     return (
       <div className="App">
-        <h1>Hello, buddy!</h1>
+        <h1>Hello, {this.state.name}!</h1>
         <h2>The time is - {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 export default App;
-
