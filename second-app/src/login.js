1a1e53ee64eb21b6d4d4a4421332cbbace2a80ef
Vishnu A V <vishnu.av@perfomatix.com>
2023-03-07 21:26:05 +0530

    Inline If with Logical && Operator Preventing Component from Rendering

--- second-app/src/login.js
+++ second-app/src/login.js
@@ -5,15 +5,15 @@ export default class LoginControl extends React.Component {
     super(props);
     this.handleLoginClick = this.handleLoginClick.bind(this);
     this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
+    this.state = { isLoggedIn: false };
   }
 
   handleLoginClick() {
-    this.setState({isLoggedIn: true});
+    this.setState({ isLoggedIn: true });
   }
 
   handleLogoutClick() {
-    this.setState({isLoggedIn: false});
+    this.setState({ isLoggedIn: false });
   }
 
   render() {
@@ -29,31 +29,25 @@ export default class LoginControl extends React.Component {
       <div>
         <Greeting isLoggedIn={isLoggedIn} />
         {button}
+        {!isLoggedIn && <a>forgot password?</a>}
       </div>
     );
   }
 }
 function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
-  );
+  return <button onClick={props.onClick}>Login</button>;
 }
 
 function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
+  return <button onClick={props.onClick}>Logout</button>;
 }
 function Greeting(props) {
   const isLoggedIn = props.isLoggedIn;
+  
   if (isLoggedIn) {
-    return <UserGreeting />;
+    return <><UserGreeting /><WarningBanner warn={isLoggedIn} /></>;
   }
-  return <GuestGreeting />;
+  return <><GuestGreeting /><WarningBanner warn={isLoggedIn} /></>;
 }
 function UserGreeting(props) {
   return <h1>Welcome back!</h1>;
@@ -61,4 +55,10 @@ function UserGreeting(props) {
 
 function GuestGreeting(props) {
   return <h1>Please sign up.</h1>;
+}
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+  return <div className="warning">Warning!</div>;
 }
