ff84491e714d024664e656994e79923ea74f7893
Vishnu A V <vishnu.av@perfomatix.com>
2023-03-07 16:18:06 +0530

    Extracting Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
 import { Profile } from "./profile";
-import {people} from './sample-data'
+import { people } from "./sample-data";
+import Comment from "./comment"
 
 function App() {
-  const name = "Vishnu";;
-      const listItems = people.map(person =>
+  const listItems = people.map((person) => (
     <li key={person.id}>
-      <Profile person={person}/>
+      <Profile person={person} />
       <p>
         <b>{person.name}</b>
-          {' ' + person.profession + ' '}
-          known for {person.accomplishment}
+        {" " + person.profession + " "}
+        known for {person.accomplishment}
       </p>
     </li>
-  );
-  return <ul>{listItems}</ul>;
+  ));
+  return (<><ul>{listItems}</ul><Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} /></>);
 }
 function formatName(user) {
   return user.firstName + " " + user.lastName;
@@ -25,15 +28,25 @@ const user = {
   lastName: "honay",
 };
 const element = (
-  <h1 className="greeting" style={{ color: "red" }} >
+  <h1 className="greeting" style={{ color: "red" }}>
     Hello, welcome!
   </h1>
 );
 function tick() {
-  return <div>
+  return (
+    <div>
       <h1>Current time</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
+  );
 }
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
 setInterval(tick, 1000);
 export default App;
