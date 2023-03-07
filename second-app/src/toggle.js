5fa588275cbbf0ae4b7f562933d3e46ed41189bb
Vishnu A V <vishnu.av@perfomatix.com>
2023-03-07 21:10:54 +0530

    Passing Arguments to Event Handlers

--- second-app/src/toggle.js
+++ second-app/src/toggle.js
@@ -1,22 +1,28 @@
 import React from "react";
 
 export default class Toggle extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {isToggleOn: true};
-    }
-  
-    handleClick = () => {
-      this.setState(prevState => ({
-        isToggleOn: !prevState.isToggleOn
-      }));
-    }
-  
-    render() {
-      return (
-        <button onClick={this.handleClick}>
-          {this.state.isToggleOn ? 'ON' : 'OFF'}
-        </button>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
   }
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  };
+  deleteRow(id) {
+    console.log(id);
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? "ON" : "OFF"}
+        </button>
+        <button onClick={(e) => this.deleteRow('1', e)}>Delete Row</button>
+      </>
+    );
+  }
+}
